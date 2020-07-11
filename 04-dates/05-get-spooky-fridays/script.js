1ea6e221dfb0ddb0aaa211c6d646a394d2d8a1eb
Amaury Henrottay <amaury.henrottay@gmail.com>
2020-07-11 22:06:37 +0200

    4.5 completed

--- 04-dates/05-get-spooky-fridays/script.js
+++ 04-dates/05-get-spooky-fridays/script.js
@@ -10,5 +10,25 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+  // your code here
+  document.getElementById("run").addEventListener("click", () => {
+    let year = document.getElementById("year").value;
+
+    month13Friday = {
+      days: 0,
+      months: [],
+    };
+
+    for (let month = 0; month < 12; month++) {
+      let date = new Date(year, month, 13);
+      if (date.getDay() == 5) {
+        month13Friday.days++;
+        month13Friday.months.push(
+          date.toLocaleDateString("default", { month: "long" })
+        );
+      }
+    }
+    console.log(month13Friday);
+    alert(Object.values(month13Friday.months));
+  });
 })();
