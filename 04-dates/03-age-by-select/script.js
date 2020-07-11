93cbe1db89a0078ad1a9e64e3deb2fe246047b92
Amaury Henrottay <amaury.henrottay@gmail.com>
2020-07-11 20:22:00 +0200

    4.3 completed

--- 04-dates/03-age-by-select/script.js
+++ 04-dates/03-age-by-select/script.js
@@ -10,5 +10,20 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+  // your code here
+  calculAge = (dob) => {
+    let diff_ms = Date.now() - dob.getTime();
+    let age = new Date(diff_ms);
+    age = Math.abs(age.getUTCFullYear() - 1970);
+    return age;
+  };
+
+  document.getElementById("run").addEventListener("click", () => {
+    let day = document.getElementById("dob-day").value;
+    let month = document.getElementById("dob-month").value;
+    let year = document.getElementById("dob-year").value;
+    let birthday = new Date(year, month, day);
+
+    console.log(calculAge(birthday));
+  });
 })();
