f9ad11149507d0708470327c44f7ddfe0f017530
Amaury Henrottay <amaury.henrottay@gmail.com>
2020-07-12 21:58:59 +0200

    7.2 completed

--- 07-classes/02-methods/script.js
+++ 07-classes/02-methods/script.js
@@ -10,5 +10,20 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+  // your code here
+  document.getElementById("run").addEventListener("click", () => {
+    class Person {
+      constructor(firstname, lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+      }
+      // Method
+      sayHello() {
+        return "Hello, " + this.firstname + " " + this.lastname + "!";
+      }
+    }
+
+    person1 = new Person("Amaury", "Henrottay");
+    console.log(person1.sayHello());
+  });
 })();
