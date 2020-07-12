bbf695fa221f6ed7dbd9efe1078d3f9599fc9fed
Amaury Henrottay <amaury.henrottay@gmail.com>
2020-07-12 19:10:06 +0200

    5.14 completed

--- 05-arrays/14-bird-names-generator/script.js
+++ 05-arrays/14-bird-names-generator/script.js
@@ -10,33 +10,50 @@
 // You will have time to focus on it later.
 
 (() => {
-    const birds = [
-        {name: "mouette", fem: true},
-        {name: "corbeau"},
-        {name: "mésange", fem: true},
-        {name: "hibou"},
-        {name: "buse", fem: true},
-        {name: "pigeon"},
-        {name: "pie", fem: true},
-        {name: "vautour"},
-        {name: "faucon"},
-        {name: "rouge-gorge"},
-        {name: "tourterelle", fem: true},
-        {name: "corneille", fem: true},
-    ];
-    const adjectives = new Set([
-        "cendré",
-        "huppé",
-        "chantant",
-        "hurlant",
-        "perché",
-        "grand",
-        "petit",
-        "bleu",
-        "pantelant",
-        "tangent",
-        "arboré",
-    ]);
+  const birds = [
+    { name: "mouette", fem: true },
+    { name: "corbeau" },
+    { name: "mésange", fem: true },
+    { name: "hibou" },
+    { name: "buse", fem: true },
+    { name: "pigeon" },
+    { name: "pie", fem: true },
+    { name: "vautour" },
+    { name: "faucon" },
+    { name: "rouge-gorge" },
+    { name: "tourterelle", fem: true },
+    { name: "corneille", fem: true },
+  ];
+  const adjectives = new Set([
+    "cendré",
+    "huppé",
+    "chantant",
+    "hurlant",
+    "perché",
+    "grand",
+    "petit",
+    "bleu",
+    "pantelant",
+    "tangent",
+    "arboré",
+  ]);
 
-    // your code here
+  // your code here
+  document.getElementById("run").addEventListener("click", () => {
+    randomIndex = (arr) => {
+      return Math.floor(Math.random() * arr.length);
+    };
+
+    let adjectivesArr = Array.from(adjectives);
+    let randomBirds = randomIndex(birds);
+    let randomAdjectives = adjectivesArr[randomIndex(adjectivesArr)];
+
+    if (birds[randomBirds].fem === true) {
+      document.getElementById("target").innerText =
+        birds[randomBirds].name + " " + randomAdjectives + "e";
+    } else {
+      document.getElementById("target").innerText =
+        birds[randomBirds].name + " " + randomAdjectives;
+    }
+  });
 })();
