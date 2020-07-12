1c4265ece406f8b2ab35321b676130e2a9e9dc8d
Amaury Henrottay <amaury.henrottay@gmail.com>
2020-07-12 21:49:48 +0200

    7.1 completed

--- 07-classes/01-instances/script.js
+++ 07-classes/01-instances/script.js
@@ -10,11 +10,17 @@
 // You will have time to focus on it later.
 
 (() => {
-    class Cat {
-        constructor(name, age) {
-            this.name = name;
-            this.age = age;
-        }
+  class Cat {
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
     }
-    // your code here
+  }
+  // your code here
+  document.getElementById("run").addEventListener("click", () => {
+    catOne = new Cat((name = "Skitty"), (age = 9));
+    catTwo = new Cat("Pixel", 6);
+    console.log(catOne);
+    console.log(catTwo);
+  });
 })();
