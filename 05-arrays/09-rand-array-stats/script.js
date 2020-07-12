a4e863304f01030805a716884576dd62661754af
Amaury Henrottay <amaury.henrottay@gmail.com>
2020-07-12 02:10:22 +0200

    5.9 completed

--- 05-arrays/09-rand-array-stats/script.js
+++ 05-arrays/09-rand-array-stats/script.js
@@ -10,5 +10,30 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+  // your code here
+  getRandomNumber = () => {
+    let nombre = Math.floor(Math.random() * 100 + 1);
+    return nombre;
+  };
+
+  document.getElementById("run").addEventListener("click", () => {
+    let listNumber = [];
+    for (let i = 1; i < 11; i++) {
+      listNumber.push(getRandomNumber());
+      document.getElementById("n-" + i).innerText = listNumber[i - 1];
+    }
+
+    document.getElementById("min").innerText = Math.min(...listNumber);
+    document.getElementById("max").innerText = Math.max(...listNumber);
+
+    document.getElementById("sum").innerText = listNumber.reduce(
+      (acc, currentValue) => acc + currentValue,
+      0
+    );
+    document.getElementById("average").innerText = listNumber.reduce(
+      (acc, currentValue) => acc + currentValue / listNumber.length,
+      0
+    );
+    // document.getElementById("min").innerText = Math.max(...listNumber);
+  });
 })();
