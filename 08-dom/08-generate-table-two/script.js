bda8af6b376cf182c918829dde54578285754ce2
Amaury Henrottay <amaury.henrottay@gmail.com>
2020-07-13 20:04:23 +0200

    8.8 completed

--- 08-dom/08-generate-table-two/script.js
+++ 08-dom/08-generate-table-two/script.js
@@ -10,5 +10,17 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+  // your code here
+  let target = document.getElementById("target");
+  let table = document.createElement("table");
+  target.appendChild(table);
+  for (let i = 1; i <= 10; i++) {
+    let row = document.createElement("tr");
+    table.appendChild(row);
+    for (let j = 1; j <= 10; j++) {
+      let col = document.createElement("td");
+      col.innerText = `${i} * ${j} = ${i * j} `;
+      row.appendChild(col);
+    }
+  }
 })();
