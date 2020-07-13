e99ef82ad3788907926b273f0941b74b5d931c14
Amaury Henrottay <amaury.henrottay@gmail.com>
2020-07-13 07:59:39 +0200

    8.3 completed

--- 08-dom/03-select-three/script.js
+++ 08-dom/03-select-three/script.js
@@ -10,5 +10,9 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+  // your code here
+
+  for (let i = 0; i < 5; i++) {
+    document.getElementsByClassName("target")[i].innerHTML = "owned";
+  }
 })();
