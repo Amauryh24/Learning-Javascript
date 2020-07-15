b06b919b680d5b675f75ece66c968179dcde7429
Amaury Henrottay <amaury.henrottay@gmail.com>
2020-07-15 22:02:47 +0200

    9.1 step

--- 09-misc/01-waving-text/script.js
+++ 09-misc/01-waving-text/script.js
@@ -10,5 +10,35 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+  // your code here
+  let text = document.getElementById("target");
+  const chars = text.innerText.split("");
+  text.innerText = "";
+  let countTrick = 2;
+
+  style = (el, index, countTrick, number) => {
+    el.style.fontSize = index - countTrick + number + "px";
+    el.style.display = "inline-block";
+    el.style.transform = `translateY(${index - countTrick + number - 20}px)`;
+  };
+
+  for (let i = 0; i < chars.length; i++) {
+    let element = document.createElement("span");
+    element.innerText = chars[i];
+    text.appendChild(element);
+    if (i >= 0 && i < chars.length / 4) {
+      style(element, i, 0, 10);
+    }
+    if (i > chars.length / 4 && i < chars.length / 2) {
+      style(element, i, countTrick, 10);
+      countTrick += 2;
+    }
+    if (i > chars.length / 2 && i < (chars.length * 3) / 4) {
+      style(element, i, countTrick, 10);
+    }
+    if (i > (chars.length * 3) / 4) {
+      style(element, i, countTrick, 10);
+      countTrick += 2;
+    }
+  }
 })();
